6d9d86437d4ce5e5c5a3e8b126e0708bd66a5130
MGalusz <galuszka.mm@gmail.com>
2017-08-25 11:44:42 +0200

    Add files via upload

---
+++ bluelineMaps/bluelineMaps/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace bluelineMaps
+{
+    partial class frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.strretTextBox = new System.Windows.Forms.TextBox();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.stateTextBox = new System.Windows.Forms.TextBox();
+            this.zipCodeTextBox = new System.Windows.Forms.TextBox();
+            this.strretLabel = new System.Windows.Forms.Label();
+            this.cityLabel = new System.Windows.Forms.Label();
+            this.stateLabel = new System.Windows.Forms.Label();
+            this.zipCodeLabel = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnSearch);
+            this.splitContainer1.Panel1.Controls.Add(this.zipCodeLabel);
+            this.splitContainer1.Panel1.Controls.Add(this.stateLabel);
+            this.splitContainer1.Panel1.Controls.Add(this.cityLabel);
+            this.splitContainer1.Panel1.Controls.Add(this.strretLabel);
+            this.splitContainer1.Panel1.Controls.Add(this.zipCodeTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.stateTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.cityTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.strretTextBox);
+            this.splitContainer1.Panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(712, 454);
+            this.splitContainer1.SplitterDistance = 237;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(471, 454);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // strretTextBox
+            // 
+            this.strretTextBox.Location = new System.Drawing.Point(120, 65);
+            this.strretTextBox.Name = "strretTextBox";
+            this.strretTextBox.Size = new System.Drawing.Size(100, 29);
+            this.strretTextBox.TabIndex = 0;
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(120, 107);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(100, 29);
+            this.cityTextBox.TabIndex = 1;
+            // 
+            // stateTextBox
+            // 
+            this.stateTextBox.Location = new System.Drawing.Point(120, 142);
+            this.stateTextBox.Name = "stateTextBox";
+            this.stateTextBox.Size = new System.Drawing.Size(100, 29);
+            this.stateTextBox.TabIndex = 2;
+            // 
+            // zipCodeTextBox
+            // 
+            this.zipCodeTextBox.Location = new System.Drawing.Point(120, 175);
+            this.zipCodeTextBox.Name = "zipCodeTextBox";
+            this.zipCodeTextBox.Size = new System.Drawing.Size(100, 29);
+            this.zipCodeTextBox.TabIndex = 3;
+            // 
+            // strretLabel
+            // 
+            this.strretLabel.AutoSize = true;
+            this.strretLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.strretLabel.Location = new System.Drawing.Point(14, 70);
+            this.strretLabel.Name = "strretLabel";
+            this.strretLabel.Size = new System.Drawing.Size(55, 24);
+            this.strretLabel.TabIndex = 4;
+            this.strretLabel.Text = "street";
+            // 
+            // cityLabel
+            // 
+            this.cityLabel.AutoSize = true;
+            this.cityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cityLabel.Location = new System.Drawing.Point(14, 105);
+            this.cityLabel.Name = "cityLabel";
+            this.cityLabel.Size = new System.Drawing.Size(40, 24);
+            this.cityLabel.TabIndex = 5;
+            this.cityLabel.Text = "City";
+            // 
+            // stateLabel
+            // 
+            this.stateLabel.AutoSize = true;
+            this.stateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.stateLabel.Location = new System.Drawing.Point(14, 151);
+            this.stateLabel.Name = "stateLabel";
+            this.stateLabel.Size = new System.Drawing.Size(51, 24);
+            this.stateLabel.TabIndex = 6;
+            this.stateLabel.Text = "State";
+            // 
+            // zipCodeLabel
+            // 
+            this.zipCodeLabel.AutoSize = true;
+            this.zipCodeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.zipCodeLabel.Location = new System.Drawing.Point(12, 175);
+            this.zipCodeLabel.Name = "zipCodeLabel";
+            this.zipCodeLabel.Size = new System.Drawing.Size(89, 24);
+            this.zipCodeLabel.TabIndex = 7;
+            this.zipCodeLabel.Text = "Zip-Code";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSearch.Location = new System.Drawing.Point(100, 221);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(120, 38);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 454);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frm";
+            this.Text = "BlueLimeMaps";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label zipCodeLabel;
+        private System.Windows.Forms.Label stateLabel;
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.Label strretLabel;
+        private System.Windows.Forms.TextBox zipCodeTextBox;
+        private System.Windows.Forms.TextBox stateTextBox;
+        private System.Windows.Forms.TextBox cityTextBox;
+        private System.Windows.Forms.TextBox strretTextBox;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ bluelineMaps/bluelineMaps/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bluelineMaps
+{
+    public partial class frm : Form
+    {
+        public frm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string street = strretTextBox.Text;
+            string city = cityTextBox.Text;
+            string state = stateTextBox.Text;
+            string zipCode = zipCodeTextBox.Text;
+
+            try
+            {
+                StringBuilder queryDate = new StringBuilder();
+
+                queryDate.Append("http://maps.google.com/maps?q=");
+
+                if (street != string.Empty)
+                {
+                    queryDate.Append(street + "," + "+");
+                }
+                            if (city != string.Empty)
+                {
+                    queryDate.Append(city +","+ "+");
+                }
+                            if (state != string.Empty)
+                            {
+                                queryDate.Append(state + "," + "+");
+                            }
+                            if (zipCode != string.Empty)
+                            {
+                                queryDate.Append(zipCode + "," + "+");
+                            }
+
+                            webBrowser1.Navigate(queryDate.ToString());
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message.ToString(), "Error");
+            }
+        }
+    }
+}
